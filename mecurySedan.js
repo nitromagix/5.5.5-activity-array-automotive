8ce38ab30db762eff18be49a266678965a9c2026
Martin <nitromagix@gmail.com>
2022-04-08 22:33:16 -0700

    finished assignment

--- mecurySedan.js
+++ mecurySedan.js
@@ -2,5 +2,66 @@
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+// let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
+
+class Car extends VehicleModule.Vehicle {
+   constructor(make, model, year, color, mileage) {
+      super(make, model, year, color, mileage)
+
+      this.maximumPassengers = 5;
+      this.passenger = 0;
+      this.numberOfWheels = 4;
+      this.maximumSpeed = 160;
+      this.fuel = 10;
+      this.scheduleService = false;
+   }
+
+   // start() method already exits in the parent class
+
+   carInfo() {
+      let info = `
+         Year: ${this.year},
+         Make: ${this.make},
+         Model: ${this.model},
+         Color: ${this.color},
+         Max passengers: ${this.maximumPassengers},
+         Max speed: ${this.maximumSpeed},
+         Number of wheels: ${this.numberOfWheels},
+         Mileage: ${this.mileage},
+         Fuel: ${this.fuel}
+      `;
+      console.log(info);
+   }
+
+   loadPassenger(numberOfPassengers) {
+      this.passenger += numberOfPassengers
+   }
+
+   scheduleService() {
+      if (this.mileage > 30000) {
+         this.timeForMaintenance = true
+         console.log("It's time to bring the car in for a tune-up")
+      }
+   }
+}
+
+let c = new Car("Mecury", "A28 Sedan", 2014, "Blue", "101385");
+c.carInfo();
+c.typeOfVehicle();
+c.start();
+c.accelerate();
+c.stop();
+
+c.loadPassenger(1);
+c.start();
+c.accelerate();
+c.decelerate();
+
+var x = 0
+while (x < 10) {
+   c.accelerate();
+   x++;
+}
+
+c.stop();
--- vehicle.js
+++ vehicle.js
@@ -1,91 +1,98 @@
 class Vehicle {
-    constructor(make, model, year, color, mileage) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.passenger = 0;
-        this.speed = 0;
-        this.mileage = mileage;
-        this.started = false;
-        this.numberOfWheels = 0;
-    }
+   constructor(make, model, year, color, mileage) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.color = color;
+      this.passenger = 0;
+      this.speed = 0;
+      this.mileage = mileage;
+      this.started = false;
+      this.numberOfWheels = 0;
+   }
 
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
-    accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
-                this.fuel = this.fuel - 1;
+   start() {
+      if (this.passenger === 0) {
+         console.log("No passenger!");
+         return this.started = false;
+      }
+      if (this.fuel > 0) {
+         console.log("engine started...!!!");
+         return this.started = true;
+      } else {
+         console.log("engine cannot start...");
+         return this.started = false;
+      }
+   }
+   accelerate() {
+      if (this.started) {
+         if (this.fuel > 0) {
+            console.log(this.speed += 1);
+            this.fuel = this.fuel - 1;
+         } else {
+            console.log(`${this.make} ${this.model} is out of fuel.`);
+            this.stop();
+         }
+      } else {
+         console.log("You need to start the engine first.");
+      }
+   }
+   decelerate() {
+      if (this.started) {
+         if (this.fuel > 0) {
+            if (this.speed > 0) {
+               console.log(this.speed -= 1);
+               this.fuel = this.fuel - 1;
             } else {
-                console.log("out of fuel.");
-                this.stop();
+               console.log(`${this.make} ${this.model} has stopped moving`);
+               this.fuel = this.fuel - 1;
             }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    stop() {
-        this.started = false;
-    }
+         } else {
+            console.log(`${this.make} ${this.model} is out of fuel.`);
+            this.stop();
+         }
+      } else {
+         console.log("You need to start the engine first.");
+      }
+   }
+   stop() {
+      this.started = false;
 
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
+      this.speed > 0
+         ? console.log(`${this.make} ${this.model} has stopped.`)
+         : console.log(`${this.make} ${this.model} is already stopped.`)
+      this.speed = 0;
+   }
 
-    autoPark()
-    {
+   drive() {
+      accelerate();
+   }
+   brake() {
+      decelerate();
+   }
 
-    }
+   autoPark() {
 
-    autoDrive()
-    {
-      
-    }
+   }
 
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
-        }
-    }
+   autoDrive() {
+
+   }
+
+   typeOfVehicle(wheels) {
+      if (this.numberOfWheels == 8 && 8 == this.numberOfWheels) {
+         console.log(this.make + " " + this.model + " is a Truck");
+      } else if (this.numberOfWheels == 4 && 4 == this.numberOfWheels) {
+         console.log(this.make + " " + this.model + " is a Car");
+      } else if (this.numberOfWheels == 2 && 2 == this.numberOfWheels) {
+         console.log(this.make + " " + this.model + " is a Bike");
+      } else {
+         console.log("Unknown type of vehicle");
+      }
+   }
 }
 
 //This exports things you want to use from this "module", more info in readme
 module.exports = {
-    Vehicle
+   Vehicle
 }
